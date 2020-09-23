7c62a04aa8ea10f92e7bda22e2be13048ea747ab
张登川 <zhangdengchuan@zhangdengchuandeMacBook-Pro.local>
2020-09-23 16:49:14 +0800

    从0开始

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>狗测试</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          狗测试
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
